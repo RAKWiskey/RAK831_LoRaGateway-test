07ad376d2454ef570914e9c47a6fa7f03cba8101
chace <xiaocheng.cao@rakwireless.com>
2017-07-15 01:15:32 -0700

    updata

---
+++ lora_gateway/libloragw/inc/config.h
@@ -0,0 +1,15 @@
+#ifndef _LORAGW_CONFIGURATION_H
+#define _LORAGW_CONFIGURATION_H
+	#define LIBLORAGW_VERSION	"1.5.0"
+	#define CFG_SPI_FTDI	1
+	#define CFG_CHIP_1301	1
+	#define CFG_RADIO_AUTO	1
+	#define CFG_BAND_FULL	1
+	#define CFG_BRD_1301REF433_V2	1
+	#define CFG_NET_PRIVATE	1
+	#define DEBUG_AUX	0
+	#define DEBUG_SPI	0
+	#define DEBUG_REG	0
+	#define DEBUG_HAL	0
+	#define DEBUG_GPS	0
+#endif
